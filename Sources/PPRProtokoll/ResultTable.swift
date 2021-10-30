12afb4ed337ed967c940243ec57c988deeeea01d
simon <simon2204s@gmail.com>
2021-10-31 01:38:06 +0200

    .

--- Sources/PPRProtokoll/ResultTable.swift
+++ Sources/PPRProtokoll/ResultTable.swift
@@ -1,30 +1,55 @@
 import SwiftPDFUI
 
 struct ResultTable: View {
+	
+	let grayBackgroundColor = Color(red: 0.98, green: 0.98, blue: 0.98)
+	
 	var body: some View {
 		VStack(spacing: 0) {
 			
 			HorizontalDivider()
 			
+			TableRow {
+				Text("Testfall")
+					.fontSize(13)
+			} trailing: {
+				Text("Status")
+					.fontSize(13)
+			}
+			.background {
+				grayBackgroundColor
+			}
+
+			HorizontalDivider()
+			
 			ForEach(0..<10) { _ in
 				TableRow {
-					Text("Hallo, Welt!")
+					Text("Hallo, Welt!").fontSize(10)
 				} trailing: {
 					SuccessTag()
 				}
+				
+				HorizontalDivider()
 			}
 			
 			TableRow {
-				Text("Hallo, Welt!")
+				Text("Hallo, Welt!").fontSize(10)
 			} trailing: {
 				FailureTag()
 			}
+			
+			HorizontalDivider()
 
 			TableRow {
-				Text("Hallo, Welt!")
+				Text("Hallo, Welt!").fontSize(10)
 			} trailing: {
 				ErrorTag()
 			}
+			.background {
+				grayBackgroundColor
+			}
+			
+			HorizontalDivider()
 		}
 	}
 }
@@ -41,21 +66,14 @@ struct TableRow<Leading, Trailing>: View where Leading: View, Trailing: View {
 	}
 	
 	var body: some View {
-		
 		VStack(spacing: 0) {
-			
 			HStack {
-				
 				leadingItem
 					.padding(16)
-				
 				Spacer()
-				
 				trailingItem
 					.frame(width: 120, alignment: .leading)
 			}
-			
-			HorizontalDivider()
 		}
 	}
 }
