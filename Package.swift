64f4b6aa35f0f62c7b60c3c30960c4a7de85fd07
simon <simon2204s@gmail.com>
2021-12-16 18:44:49 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftPDF",
-	platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15), .iOS(.v15)],
     products: [
         .library(
             name: "SwiftPDF",
